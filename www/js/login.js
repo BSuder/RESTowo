18ea13f05bd258cee7d2aa4bd626bffe0ccdeced
Unknown <suderbartek@gmail.com>
2018-05-05 22:48:56 +0200

    googlowy button

--- www/js/login.js
+++ www/js/login.js
@@ -95,3 +95,29 @@ window.onGoogleYoloLoad = (googleyolo) => {
 	alert("google yolo");
 	// The 'googleyolo' object is ready for use.
 };
+/*
+
+var googleUser = {};
+  var startApp = function() {
+    gapi.load('auth2', function(){
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      auth2 = gapi.auth2.init({
+        client_id: '686795737390-u2o1airh1e14jcm58cejmhihnfsn1991.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        // Request scopes in addition to 'profile' and 'email'
+        //scope: 'additional_scope'
+      });
+      attachSignin(document.getElementById('customBtn'));
+    });
+  };
+
+  function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {},
+        function(googleUser) {
+          document.getElementById('name').innerText = "Signed in: " +
+              googleUser.getBasicProfile().getName();
+        }, function(error) {
+          alert(JSON.stringify(error, undefined, 2));
+        });
+  }*/
